f30b3d72dda29bae58f1d05e659232ea3174f601
jerry <614237720@qq.com>
2019-01-02 14:25:56 +0800

    操作APP并获取内存增长情况

--- appdumphead.sh
+++ appdumphead.sh
@@ -9,3 +9,14 @@ adb shell rm -rf /data/local/tmp/dumpheap/
 #win不行会报路径错误
 echo "创建新的dumpheap文件目录"
 adb shell mkdir /data/local/tmp/dumpheap/
+
+
+#需要修改对应的操控，使用Mac
+for (( i = 0; i < ${swipe_count}; i++ )); do
+    echo "向下滑动列表"
+    adb shell input swipe 700 2000 700 200
+    adb shell am dumpheap com.xueqiu.android /data/local/tmp/dumpheap/dumpheap_${i}
+    sleep 3
+    adb pull /data/local/tmp/dumpheap/dumpheap_${i} `pwd`/test/dumpheap_${i}
+    hprof-conv `pwd`/test/dumpheap_${i} `pwd`/test/dumpheap_${i}.jvm.hprof
+done
